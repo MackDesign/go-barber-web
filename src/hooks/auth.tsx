d9d47ebef50c7ff643701df2f38ccb12dc9ce94e
Mackdesign <marcio@design2u.com.br>
2020-10-25 02:39:54 -0300

    Dashboard

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,10 +1,17 @@
+/* eslint-disable camelcase */
 import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
+
+interface User {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
 
 interface AuthState {
   token: string;
   // eslint-disable-next-line @typescript-eslint/ban-types
-  user: object;
+  user: User;
 }
 
 interface SignInCredentials {
@@ -14,7 +21,7 @@ interface SignInCredentials {
 
 interface AuthContextData {
   // eslint-disable-next-line @typescript-eslint/ban-types
-  user: object;
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
@@ -27,6 +34,8 @@ const AuthProvider: React.FC = ({ children }) => {
     const user = localStorage.getItem('@GoBarber:user');
 
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
       return { token, user: JSON.parse(user) };
     }
 
@@ -43,6 +52,8 @@ const AuthProvider: React.FC = ({ children }) => {
 
     localStorage.setItem('@GoBarber:token', token);
     localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
